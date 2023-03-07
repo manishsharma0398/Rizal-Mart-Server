838e0cb7442c62248f85b224781d4e5907ba3b79
Manish Sharma <manish.sharma0398@gmail.com>
2023-03-07 10:14:15 +0530

    allowed vercel frontend cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,9 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://rizal-mart-front-end.vercel.app",
+  "https://rizal-mart-front-end.vercel.app/",
+  "https://rizal-mart-front-end-manishsharma0398.vercel.app/",
+  "https://rizal-mart-front-end-manishsharma0398.vercel.app",
+];
 
 module.exports = allowedOrigins;
